049bd275e823c1a055afe2d81fb6618616eb34f9
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-10 00:17:46 +0530

    Started working on the app backend api

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ const fetch = require("node-fetch");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 const options = {
   method: "GET",
   headers: {
@@ -11,14 +12,30 @@ const options = {
     "X-RapidAPI-Host": process.env.HOST,
   },
 };
+const options2 = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.API_KEY,
+    "X-RapidAPI-Host": process.env.AIR_HOST,
+  },
+};
 
 app.get("/api/search", (req, res) => {
   let { city } = req.query;
+  let { country } = req.query;
   const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
-  fetch(url, options)
-    .then((res) => res.json())
-    .then((json) => res.json(json))
-    .catch((err) => console.error("error:" + err));
+  const url2 = `https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=${city}&country=${country}`;
+  if (city) {
+    fetch(url, options)
+      .then((re) => re.json())
+      .then((json) => res.json(json))
+      .catch((err) => console.error(err));
+  } else if (city && country) {
+    fetch(url2, options2)
+      .then((re) => re.json())
+      .then((json) => res.json(json))
+      .catch((err) => console.error(err));
+  }
 });
 
 app.listen(port, () => {
